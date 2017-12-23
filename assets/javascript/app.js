20084549d9bb20c8ac21c370aa60819ec465bf51
Caleb Rose <clrose86@gmail.com>
2017-12-23 02:28:56 -0600

    added js

---
+++ assets/javascript/app.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCBLeThRzde99KzvAy-Uc7-x9anNey4x9A",
+    authDomain: "fire-train.firebaseapp.com",
+    databaseURL: "https://fire-train.firebaseio.com",
+    projectId: "fire-train",
+    storageBucket: "",
+    messagingSenderId: "962106421042"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  var tName;
+  var destination;
+  var timeBetween;
+  var firstTrain;
+
+  $("#addTrain").on("click",function(){
+  	event.preventDefault();
+  	tName = $("#trainName").val().trim();
+  	console.log("new T name: "+tName);
+  	destination = $("#trainDest").val().trim();
+  	console.log("headed to: "+destination);
+  	firstTrain = $("#trainStart").val().trim();
+  	console.log("first train time: "+firstTrain);
+  	timeBetween = $("#trainFreq").val().trim();
+  	console.log("how long between: "+timeBetween);
+
+  	database.ref().push({
+  		name: tName,
+  		destination: destination,
+  		first: firstTrain,
+  		frequency: timeBetween
+  	});
+  });
+
+  database.ref().on("child_added",function(update){
+  	console.log(update.val());
+  	var tableRow=$("<tr>");
+  	tableRow.append("<td>"+update.val().name+"</td>");
+  	tableRow.append("<td>"+update.val().destination+"</td>");
+  	tableRow.append("<td>"+update.val().frequency+" mins </td>");
+  	var firstTrainConveert = moment(firstTrain,"hh:mm").subtract(1,"years");
+  	console.log(firstTrain)
+  	$("tbody").append(tableRow);
+  });
+
+})
